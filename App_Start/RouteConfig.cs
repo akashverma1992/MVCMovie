8a86e06b8552dfc4404563078b51c5b5d2a3ef22
Aakash <av8800919186@gmail.com>
2018-01-06 23:23:39 +0530

    Add files via upload

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVCMovie
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "home", action = "index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name:"hello",
+                url: "{controller}/{action}/{id}",
+                defaults: new {
+                    controller = "helloworld",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                }
+            );
+
+            routes.MapRoute(
+                name: "hello2",
+                url: "{controller}/{action}/{name}/{age}"
+            );
+
+            routes.MapRoute(
+                name: "Movies",
+                url: "{controller}/{action}/{id}",
+                defaults: new {
+                    controller = "Movies",
+                    action = "Edit",
+                    id = UrlParameter.Optional
+                }
+            );
+        }
+    }
+}
