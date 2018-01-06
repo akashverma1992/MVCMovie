2faeba2374a3fe791736d94c152beac9c8034b4e
Aakash <av8800919186@gmail.com>
2018-01-06 23:25:17 +0530

    Add files via upload

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+
+        public ActionResult Index()
+        {
+            //return View();
+            //ActionResult ar;
+            //ar = (ViewResult) new ViewResult().View;
+            return View();
+        }
+
+        public ActionResult Welcome(String name, int numTimes = 1)
+        {
+            ViewBag.Name = name;
+            ViewBag.Message = "Hello, " + name;
+            ViewBag.NumTime = numTimes;
+            return View();
+        }
+        //public String Index2()
+        //{
+        //    return "<b>This is default action.....</b>";
+        //}
+
+        //public String Welcome1()
+        //{
+        //    return "this is the <b>welcome action method.....</b>";
+        //}
+
+        //public String Welcome2(String name, int age = 0)
+        //{
+        //    string str = HttpUtility
+        //        .HtmlEncode("Hello " + name + ", age is " + age);
+        //    return str;
+        //}
+
+        public ActionResult googlePage()
+        {
+            return Redirect("http://www.google.co.in/");
+        }
+    }
+}
